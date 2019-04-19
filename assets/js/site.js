1da62ea75d0febb228576147bb65ee0e8deb6041
Stive Tenorio <stivetenorio3520@yahoo.com>
2019-04-18 20:14:34 -0500

    Started work on js and cart page

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,4 +4,16 @@ var html = document.querySelector('html');
 html.classList.remove('nojs');
 html.classList.add('js');
 
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
 //cart
+var cartItems = localStorage.setItem('shop-item-title', 'item-price');
+
+cartItems.push(newItem);
+
+localStorage.setItem('shop-item-title', 'item-price');
+
+var cartItems = localStorage.getItem('shop-item-title', 'item-price');
