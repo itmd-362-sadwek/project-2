bbc438b6830a417b2c717a59b7f96c29ce5e47fe
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2019-04-21 20:01:31 -0500

    Add 60616.js for Fetch API

---
+++ assets/js/60616.js
@@ -0,0 +1,1 @@
+{"post code": "60616", "country": "The United States of America", "country abbreviation": "US", "places": [{"place name": "Chicago", "longitude": "-87.6306", "state": "Illinois", "state abbreviation": "IL", "latitude": "41.8426"}]}
