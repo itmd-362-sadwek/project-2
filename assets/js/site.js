7304c3215b71b0ca8cd4a4edcd894b131f529ba2
Kelly <kdenisse29@gmail.com>
2019-04-21 21:06:47 -0500

    totals

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,11 +3,8 @@
 var html = document.querySelector('html');
 html.classList.remove('nojs');
 html.classList.add('js');
-<<<<<<< HEAD
-=======
 
 //cart
->>>>>>> 6b4845160b16411bb6d8bd393f52c4320b42f7e2
 /* Set rates + misc */
 var taxRate = 0.05;
 var shippingRate = 15.00; 
